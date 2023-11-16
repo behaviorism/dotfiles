a25db235742f11b311525458ab0c89502c6f6ed0
behaviorism <luka.debelic03@gmail.com>
2023-11-16 04:16:01 +0100

    updated dotfiles

--- upload.sh
+++ upload.sh
@@ -2,10 +2,10 @@ cd ~/dotfiles
 
 copy_folder() {
 	mkdir -p `dirname ~/test/"$1"`
-	cp -r ~/${~1} `dirname ~/test/"$1"`
+	cp -r ~/$1 `dirname ~/test/"$1"`
 }
 
-paths=('.skhdrc' '.tmux.conf' '.yabairc' '.zshrc' '.config/alacritty' '.vim/{.,}*' 'typescript')
+paths=('.skhdrc' '.tmux.conf' '.yabairc' '.zshrc' '.config/alacritty' '.vim/*.*' 'typescript')
 
 for p in $paths; do
 	copy_folder $p
