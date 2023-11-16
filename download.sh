f6199ee357ae433853eec846e28dad74997162d9
behaviorism <luka.debelic03@gmail.com>
2023-11-16 01:15:29 +0100

    updated dotfiles

--- download.sh
+++ download.sh
@@ -2,4 +2,4 @@ cd ~/dotfiles
 
 git pull origin master
 
-find ~/dotfiles ! -name 'download.sh' ! -name 'upload.sh' ! -path '*.git*' -mindepth 1 -exec cp -r {} ~ \;
+find ~/dotfiles/ ! -name 'download.sh' ! -name 'upload.sh' ! -path '*.git*' -maxdepth 1 -exec cp -r {} ~/test \;
