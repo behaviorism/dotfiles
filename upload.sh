cd40083137e12ca4db0b724779cc66d0be2060a9
behaviorism <luka.debelic03@gmail.com>
2023-11-16 04:16:23 +0100

    updated dotfiles

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@ cd ~/dotfiles
 
 copy_folder() {
 	mkdir -p `dirname ~/test/"$1"`
-	cp -r ~/$1 `dirname ~/test/"$1"`
+	cp -r ~/${1} `dirname ~/test/"$1"`
 }
 
 paths=('.skhdrc' '.tmux.conf' '.yabairc' '.zshrc' '.config/alacritty' '.vim/*.*' 'typescript')
