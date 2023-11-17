5dbc3ebb5dd1dc04b81ec32904112aa3cf43eeee
behaviorism <luka.debelic03@gmail.com>
2023-11-17 20:12:11 +0100

    updated dotfiles

--- scripts/square_corners.sh
+++ scripts/square_corners.sh
@@ -2,6 +2,8 @@ git clone https://github.com/tsujp/customise_macos
 
 mkdir -p /Users/kappa/livemount
 
-sudo cp ~/customise_macos/appearance/DarkAquaAppearance/Edited/DarkAquaAppearance.car /Users/kappa/livemount/System/Library/CoreServices/SystemAppearance.bundle/Contents/Resources/DarkAqua.car
+sudo mount -o nobrowse -t apfs  /dev/disk1s1 /Users/kappa/livemount
+
+sudo cp ~/dotfiles/scripts/DarkAqua.car /Users/kappa/livemount/System/Library/CoreServices/SystemAppearance.bundle/Contents/Resources/DarkAqua.car
 
 sudo bless --mount /Users/kappa/livemount --bootefi --create-snapshot
